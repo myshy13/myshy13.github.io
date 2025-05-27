57a8284383b92113b8727f43b8b8154ccc396e41
hamish.oliver@icloud.com <hamish.oliver@icloud.com>
2025-05-27 17:29:36 +1000

    added a very buggy search

---
+++ search.js
@@ -0,0 +1,108 @@
+const games = [
+  "jelly truck",
+  "slope",
+  "minecraft",
+  "run 3",
+  "mr mine",
+  "moto x3m",
+  "opposite day",
+  "2048",
+  "cookie clicker",
+  "tiny fishing",
+];
+
+let params = new URLSearchParams(new URL(window.location.href).search);
+let searchQuery = params.get("search");
+
+function filterArray(array, searchString) {
+  return array.filter((str) => str.includes(searchString));
+}
+
+if (searchQuery != "" && searchQuery != null) {
+  console.log("seaching...");
+  console.log("result: " + searchQuery);
+
+  const results = filterArray(games, searchQuery);
+
+  console.log(results);
+
+  if ((results.length = 1)) {
+    switch (results[0]) {
+      case "jelly truck":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/jellytruck/index.html"}`
+        );
+        break;
+      case "minecraft":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/minecraft/index.html"}`
+        );
+      case "moto":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/moto/x3m/index.html"}`
+        );
+      case "mr mine":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/mrmine/index.html"}`
+        );
+        break;
+      case "slope":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/slope/index.html"}`
+        );
+        break;
+      case "run 3":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/run3/index.html"}`
+        );
+        break;
+      case "opposite day":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/oppositeday/index.html"}`
+        );
+        break;
+      case "2048":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/2048/index.html"}`
+        );
+        break;
+      case "cookie clicker":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/cookieclicker/index.html"}`
+        );
+        break;
+      case "tiny fishing":
+        window.location.assign(
+          `${document.location.protocol}//${document.location.hostname}:${
+            document.location.port
+          }${"/src/tinyFishing/index.html"}`
+        );
+        break;
+      default:
+        const prev = document.getElementsByClassName("content")[0].innerHTML;
+        document.getElementsByClassName("content")[0].innerHTML = "";
+        document.getElementsByClassName(
+          "content"
+        )[0].innerHTML = `<h1 style="color:white;display:block;">no results</h1><div class="content">${prev}<div>`;
+        break;
+    }
+  }
+}
