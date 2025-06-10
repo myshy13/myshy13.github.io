66ff44a0d826dacd0fa1aa90cee223957b1344ea
hamish.oliver@icloud.com <hamish.oliver@icloud.com>
2025-06-10 18:04:54 +1000

    added the world's hardest game

--- assets/search.js
+++ assets/search.js
@@ -29,6 +29,10 @@ const games = [
   "fireboy and watergirl 2",
   "fireboy and watergirl and the light temple",
   "tetris",
+  "mariokart",
+  "pacman",
+  "the world's hardest game",
+  "the worlds hardest game",
 ];
 
 let params = new URLSearchParams(new URL(window.location.href).search);
@@ -38,247 +42,262 @@ function filterArray(array, searchString) {
   return array.filter((str) => str.includes(searchString));
 }
 
-if (searchQuery != "" && searchQuery != null) {
-  console.log("seaching...");
-  console.log("result: " + searchQuery);
+const results = filterArray(games, searchQuery.toLowerCase());
+if (searchQuery === null || searchQuery === "") {
+  document.getElementsByClassName(
+    "content"
+  )[0].innerHTML = `<h1 style="color:white;display:block;">no results</h1>`;
+}
 
-  const results = filterArray(games, searchQuery);
+console.log("searchQuery", searchQuery);
+console.log("results: " + results);
 
-  console.log(results);
-
-  if ((results.length = 1)) {
-    switch (results[0]) {
-      case "jelly truck":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/jellytruck/index.html"}`
-        );
-        break;
-      case "pacman":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/pacman/index.html"}`
-        );
-        break;
-      case "mariokart":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/mariokart/index.html"}`
-        );
-        break;
-      case "tetris":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/tetris/index.html"}`
-        );
-        break;
-      case "fireboy and watergirl":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/fireboyandwatergirllighttemple/index.html"}`
-        );
-        break;
-      case "fire boy and water girl":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/fireboyandwatergirllighttemple/index.html"}`
-        );
-        break;
-      case "fire boy and water girl 2":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/fireboyandwatergirllighttemple/index.html"}`
-        );
-        break;
-      case "fire boy and water girl and the light temple":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/fireboyandwatergirllighttemple/index.html"}`
-        );
-        break;
-      case "mini putt":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/miniputt/index.html"}`
-        );
-        break;
-      case "mini golf":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/miniputt/index.html"}`
-        );
-        break;
-      case "papa's freezeria":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/papasfreezieria/index.html"}`
-        );
-        break;
-      case "papas freezeria":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/papasfreezieria/index.html"}`
-        );
-        break;
-      case "the impossible quiz":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/impossiblequiz/index.html"}`
-        );
-        break;
-      case "papa's pizzeria":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/papaspizzeria/index.html"}`
-        );
-        break;
-      case "angry gran 2":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/angrygran2/index.html"}`
-        );
-        break;
-      case "papas pizzeria":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/papaspizzeria/index.html"}`
-        );
-        break;
-      case "minecraft":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/minecraft/index.html"}`
-        );
-        break;
-      case "moto x3m":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/moto/x3m/index.html"}`
-        );
-        break;
-      case "mr mine":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/mrmine/index.html"}`
-        );
-        break;
-      case "slope":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/slope/index.html"}`
-        );
-        break;
-      case "slope 3":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/slope3/index.html"}`
-        );
-        break;
-      case "run 3":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/run3/index.html"}`
-        );
-        break;
-      case "opposite day":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/oppositeday/index.html"}`
-        );
-        break;
-      case "2048":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/2048/index.html"}`
-        );
-        break;
-      case "cookie clicker":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/cookieclicker/index.html"}`
-        );
-        break;
-      case "tiny fishing":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/tinyFishing/index.html"}`
-        );
-        break;
-      case "hexanaut.io":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/hexanautIO/index.html"}`
-        );
-        break;
-      case "a small world cup":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/asmallworldcup/index.html"}`
-        );
-        break;
-      case "asmallworldcup":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/asmallworldcup/index.html"}`
-        );
-        break;
-      case "bitlife":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/bitlife/index.html"}`
-        );
-        break;
-      case "bitlife simulator":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/bitlife/index.html"}`
-        );
-        break;
-      case "subway surfers":
-        window.location.assign(
-          `${document.location.protocol}//${document.location.hostname}:${
-            document.location.port
-          }${"/src/subwaysurfers/index.html"}`
-        );
-        break;
-      default:
-        const prev = document.getElementsByClassName("content")[0].innerHTML;
-        document.getElementsByClassName("content")[0].innerHTML = "";
-        document.getElementsByClassName(
-          "content"
-        )[0].innerHTML = `<h1 style="color:white;display:block;">no results</h1><div class="content">${prev}<div>`;
-        break;
-    }
+if ((results.length = 1)) {
+  switch (results[0]) {
+    case "jelly truck":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/jellytruck/index.html"}`
+      );
+      break;
+    case "pacman":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/pacman/index.html"}`
+      );
+      break;
+    case "the world's hardest game":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/hardestgame/index.html"}`
+      );
+      break;
+    case "the worlds hardest game":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/hardestgame/index.html"}`
+      );
+      break;
+    case "mariokart":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/mariokart/index.html"}`
+      );
+      break;
+    case "tetris":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/tetris/index.html"}`
+      );
+      break;
+    case "fireboy and watergirl":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/fireboyandwatergirllighttemple/index.html"}`
+      );
+      break;
+    case "fire boy and water girl":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/fireboyandwatergirllighttemple/index.html"}`
+      );
+      break;
+    case "fire boy and water girl 2":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/fireboyandwatergirllighttemple/index.html"}`
+      );
+      break;
+    case "fire boy and water girl and the light temple":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/fireboyandwatergirllighttemple/index.html"}`
+      );
+      break;
+    case "mini putt":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/miniputt/index.html"}`
+      );
+      break;
+    case "mini golf":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/miniputt/index.html"}`
+      );
+      break;
+    case "papa's freezeria":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/papasfreezieria/index.html"}`
+      );
+      break;
+    case "papas freezeria":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/papasfreezieria/index.html"}`
+      );
+      break;
+    case "the impossible quiz":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/impossiblequiz/index.html"}`
+      );
+      break;
+    case "papa's pizzeria":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/papaspizzeria/index.html"}`
+      );
+      break;
+    case "angry gran 2":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/angrygran2/index.html"}`
+      );
+      break;
+    case "papas pizzeria":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/papaspizzeria/index.html"}`
+      );
+      break;
+    case "minecraft":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/minecraft/index.html"}`
+      );
+      break;
+    case "moto x3m":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/moto/x3m/index.html"}`
+      );
+      break;
+    case "mr mine":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/mrmine/index.html"}`
+      );
+      break;
+    case "slope":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/slope/index.html"}`
+      );
+      break;
+    case "slope 3":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/slope3/index.html"}`
+      );
+      break;
+    case "run 3":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/run3/index.html"}`
+      );
+      break;
+    case "opposite day":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/oppositeday/index.html"}`
+      );
+      break;
+    case "2048":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/2048/index.html"}`
+      );
+      break;
+    case "cookie clicker":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/cookieclicker/index.html"}`
+      );
+      break;
+    case "tiny fishing":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/tinyFishing/index.html"}`
+      );
+      break;
+    case "hexanaut.io":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/hexanautIO/index.html"}`
+      );
+      break;
+    case "a small world cup":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/asmallworldcup/index.html"}`
+      );
+      break;
+    case "asmallworldcup":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/asmallworldcup/index.html"}`
+      );
+      break;
+    case "bitlife":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/bitlife/index.html"}`
+      );
+      break;
+    case "bitlife simulator":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/bitlife/index.html"}`
+      );
+      break;
+    case "subway surfers":
+      window.location.assign(
+        `${document.location.protocol}//${document.location.hostname}:${
+          document.location.port
+        }${"/src/subwaysurfers/index.html"}`
+      );
+      break;
+    default:
+      const prev = document.getElementsByClassName("content")[0].innerHTML;
+      document.getElementsByClassName("content")[0].innerHTML = "";
+      document.getElementsByClassName(
+        "content"
+      )[0].innerHTML = `<h1 style="color:white;display:block;">no results</h1><div class="content">${prev}<div>`;
+      break;
   }
 }
